c42f6eec418024c35c0b1ecb5a934497ca35f957
greg <kajin41@gmail.com>
2017-06-30 00:13:01 -0400

    Initial commit

---
+++ config.py
@@ -0,0 +1,12 @@
+# email server
+MAIL_SERVER = 'smtp.googlemail.com'
+MAIL_PORT = 465
+MAIL_USE_TLS = False
+MAIL_USE_SSL = True
+MAIL_USERNAME = 'kajin41@gmail.com'
+MAIL_PASSWORD = 'furuirifi&r2b2rfrib2rfir'
+
+AWS_PHOTO_KEY = 'AKIAI4KYI5TMHRPBPXZQ'
+AWS_PHOTO_SECRET = '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'
+# administrator list
+ADMINS = ['greg@gregorymercado.com']
---
+++ wsgi.py
@@ -0,0 +1,6 @@
+__author__ = 'greg'
+from resume import app
+from photo import photo_app
+if __name__ == "__main__":
+    app.run()
+    photo_app.run()
