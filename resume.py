6107c58e623bf58d069e86254b719adb1caf22ca
greg <kajin41@gmail.com>
2017-09-11 15:52:09 -0400

    added captcha

--- resume.py
+++ resume.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request, redirect
 from mail import signup_thank_you, contact_email
 from database import create_signup_entry, create_contact_entry
+import requests
+import config
+import json
 
 app = Flask(__name__)
 
@@ -15,23 +18,50 @@ def index():
 
 @app.route('/signup', methods=["POST"])
 def patient_signup():
-    name = request.form['name']
-    email = request.form['email']
-    signup_type = request.form['type']
-    create_signup_entry(name, email, signup_type)
-    signup_thank_you(name, email, signup_type)
-    return "Thanks For Signing up, You Will Receive an e-Mail Shortly"
+    r = requests.post('https://www.google.com/recaptcha/api/siteverify',
+                      data={'secret': config.captcha_secret_key,
+                            'response': request.form['g-recaptcha-response'],
+                            'remoteip': request.headers['X-Forwarded-For']})
+
+    google_response = json.loads(r.text)
+    print('JSON: ', google_response)
+
+    if google_response['success']:
+        print('captcha SUCCESS')
+        name = request.form['name']
+        email = request.form['email']
+        signup_type = request.form['type']
+        create_signup_entry(name, email, signup_type)
+        signup_thank_you(name, email, signup_type)
+        return "Thanks For Signing up, You Will Receive an e-Mail Shortly"
+    else:
+        return "oops something went wrong."
 
 
 @app.route('/contact', methods=["POST"])
 def contact():
-    name = request.form['name']
-    email = request.form['email']
-    subject = request.form['subject']
-    message = request.form['message']
-    create_contact_entry(name, email, subject, message)
-    contact_email(name, email, subject, message)
-    return "Thanks For Contacting Us, You Will Receive an e-Mail Shortly"
+    r = requests.post('https://www.google.com/recaptcha/api/siteverify',
+                      data={'secret': config.captcha_secret_key,
+                            'response': request.form['g-recaptcha-response'],
+                            'remoteip': request.headers['X-Forwarded-For']})
+
+    google_response = json.loads(r.text)
+    print('JSON: ', google_response)
+
+    if google_response['success']:
+        print('captcha SUCCESS')
+        name = request.form['name']
+        email = request.form['email']
+        subject = request.form['subject']
+        message = request.form['message']
+        create_contact_entry(name, email, subject, message)
+        contact_email(name, email, subject, message)
+        return "Thanks For Contacting Us, You Will Receive an e-Mail Shortly"
+    else:
+        return "oops something went wrong."
+#logic
+
+
 
 if __name__ == '__main__':
     app.run()
