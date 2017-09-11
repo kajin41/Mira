36b4db67350825b14cb0f35aef2f32660a27b248
greg <kajin41@gmail.com>
2017-09-11 18:13:30 -0400

    added captcha

--- static/js/contact_me.js
+++ static/js/contact_me.js
@@ -5,204 +5,205 @@ function doc_join(){
     $("#doctorForm").show()
 }
 
-function patient_submit() {
-            // Prevent spam click and default submit behaviour
-            $("#btnSubmit").attr("disabled", true);
-            //event.preventDefault();
-            grecaptcha.execute();
-
-            // get values from FORM
-            var name = $("input#patientname").val();
-            var email = $("input#patientemail").val();
-            var firstName = name; // For Success/Failure Message
-            // Check for white space in name for Success/Fail message
-            if (firstName.indexOf(' ') >= 0) {
-                firstName = name.split(' ').slice(0, -1).join(' ');
-            }
-            $.ajax({
-                url: "/signup",
-                type: "POST",
-                data: {
-                    name: name,
-                    email: email,
-                    type: 'patient'
-                },
-                cache: false,
-                success: function() {
-                    // Enable button & show success message
-                    $("#btnSubmit").attr("disabled", false);
-                    $('#patientsuccess').html("<div class='alert alert-success'>");
-                    $('#patientsuccess > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#patientsuccess > .alert-success')
-                        .append("<strong>You will receive an email shortly. </strong>");
-                    $('#patientsuccess > .alert-success')
-                        .append('</div>');
-
-                    //clear all fields
-                    $('#patientForm').trigger("reset");
-                },
-                error: function() {
-                    // Fail message
-                    $('#patientsuccess').html("<div class='alert alert-danger'>");
-                    $('#patientsuccess > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#patientsuccess > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
-                    $('#patientsuccess > .alert-danger').append('</div>');
-                    //clear all fields
-                    $('#patientForm').trigger("reset");
-                },
-            });
-        }
-
-        function doctor_submit() {
-            // Prevent spam click and default submit behaviour
-            $("#btnSubmit").attr("disabled", true);
-            //event.preventDefault();
-            grecaptcha.execute();
-
-
-            // get values from FORM
-            var name = $("input#doctorname").val();
-            var email = $("input#doctoremail").val();
-            var firstName = name; // For Success/Failure Message
-            // Check for white space in name for Success/Fail message
-            if (firstName.indexOf(' ') >= 0) {
-                firstName = name.split(' ').slice(0, -1).join(' ');
-            }
-            $.ajax({
-                url: "/signup",
-                type: "POST",
-                data: {
-                    name: name,
-                    email: email,
-                    type: 'doctor'
-                },
-                cache: false,
-                success: function() {
-                    // Enable button & show success message
-                    $("#btnSubmit").attr("disabled", false);
-                    $('#sdoctoruccess').html("<div class='alert alert-success'>");
-                    $('#doctorsuccess > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#doctorsuccess > .alert-success')
-                        .append("<strong>You will receive an email shortly. </strong>");
-                    $('#doctorsuccess > .alert-success')
-                        .append('</div>');
-
-                    //clear all fields
-                    $('#doctorForm').trigger("reset");
-                },
-                error: function() {
-                    // Fail message
-                    $('#doctorsuccess').html("<div class='alert alert-danger'>");
-                    $('#doctorsuccess > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#doctorsuccess > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
-                    $('#doctorsuccess > .alert-danger').append('</div>');
-                    //clear all fields
-                    $('#doctorForm').trigger("reset");
-                },
-            });
-        }
-
-        function contact_submit() {
-            // Prevent spam click and default submit behaviour
-            $("#btnSubmit").attr("disabled", true);
-            //event.preventDefault();
-            grecaptcha.execute();
-
-
-            // get values from FORM
-            var name = $("input#name").val();
-            var email = $("input#email").val();
-            var subject = $("input#subject").val();
-            var message = $("textarea#message").val();
-            var firstName = name; // For Success/Failure Message
-            // Check for white space in name for Success/Fail message
-            if (firstName.indexOf(' ') >= 0) {
-                firstName = name.split(' ').slice(0, -1).join(' ');
-            }
-            $.ajax({
-                url: "/contact",
-                type: "POST",
-                data: {
-                    name: name,
-                    email: email,
-                    subject: subject,
-                    message: message,
-                },
-                cache: false,
-                success: function() {
-                    // Enable button & show success message
-                    $("#btnSubmit").attr("disabled", false);
-                    $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-success')
-                        .append("<strong>You will receive an email shortly. </strong>");
-                    $('#success > .alert-success')
-                        .append('</div>');
-
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-                error: function() {
-                    // Fail message
-                    $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
-                    $('#success > .alert-danger').append('</div>');
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-            });
-        }
-
-$(function() {
-
-    $("#patientForm input,#patientForm textarea").jqBootstrapValidation({
-        preventSubmit: true,
-        submitError: function($form, event, errors) {
-            // additional error messages or events
-        },
-        submitSuccess: patient_submit(),
-        filter: function() {
-            return $(this).is(":visible");
-        },
-    });
-
-
-
-    $("#doctorForm input,#doctorForm textarea").jqBootstrapValidation({
-        preventSubmit: true,
-        submitError: function($form, event, errors) {
-            // additional error messages or events
-        },
-        submitSuccess: doctor_submit(),
-        filter: function() {
-            return $(this).is(":visible");
-        },
-    });
-
-
-        $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
-        preventSubmit: true,
-        submitError: function($form, event, errors) {
-            // additional error messages or events
-        },
-        submitSuccess: contact_submit(),
-        filter: function() {
-            return $(this).is(":visible");
-        },
-    });
-
-    $("a[data-toggle=\"tab\"]").click(function(e) {
-        e.preventDefault();
-        $(this).tab("show");
-    });
-});
+// function patient_submit() {
+//             // Prevent spam click and default submit behaviour
+//             $("#btnSubmit").attr("disabled", true);
+//             //event.preventDefault();
+//             grecaptcha.execute();
+//
+//             // get values from FORM
+//             var name = $("input#patientname").val();
+//             var email = $("input#patientemail").val();
+//             var firstName = name; // For Success/Failure Message
+//             // Check for white space in name for Success/Fail message
+//             if (firstName.indexOf(' ') >= 0) {
+//                 firstName = name.split(' ').slice(0, -1).join(' ');
+//             }
+//             $.ajax({
+//                 url: "/signup",
+//                 type: "POST",
+//                 data: {
+//                     name: name,
+//                     email: email,
+//                     type: 'patient',
+//                     g-recaptcha-response: "6Lc-STAUAAAAAD7uRBc_9nk8kU5JgoOPVChs5UkS"
+//                 },
+//                 cache: false,
+//                 success: function() {
+//                     // Enable button & show success message
+//                     $("#btnSubmit").attr("disabled", false);
+//                     $('#patientsuccess').html("<div class='alert alert-success'>");
+//                     $('#patientsuccess > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+//                         .append("</button>");
+//                     $('#patientsuccess > .alert-success')
+//                         .append("<strong>You will receive an email shortly. </strong>");
+//                     $('#patientsuccess > .alert-success')
+//                         .append('</div>');
+//
+//                     //clear all fields
+//                     $('#patientForm').trigger("reset");
+//                 },
+//                 error: function() {
+//                     // Fail message
+//                     $('#patientsuccess').html("<div class='alert alert-danger'>");
+//                     $('#patientsuccess > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+//                         .append("</button>");
+//                     $('#patientsuccess > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+//                     $('#patientsuccess > .alert-danger').append('</div>');
+//                     //clear all fields
+//                     $('#patientForm').trigger("reset");
+//                 },
+//             });
+//         }
+//
+//         function doctor_submit() {
+//             // Prevent spam click and default submit behaviour
+//             $("#btnSubmit").attr("disabled", true);
+//             //event.preventDefault();
+//             grecaptcha.execute();
+//
+//
+//             // get values from FORM
+//             var name = $("input#doctorname").val();
+//             var email = $("input#doctoremail").val();
+//             var firstName = name; // For Success/Failure Message
+//             // Check for white space in name for Success/Fail message
+//             if (firstName.indexOf(' ') >= 0) {
+//                 firstName = name.split(' ').slice(0, -1).join(' ');
+//             }
+//             $.ajax({
+//                 url: "/signup",
+//                 type: "POST",
+//                 data: {
+//                     name: name,
+//                     email: email,
+//                     type: 'doctor'
+//                 },
+//                 cache: false,
+//                 success: function() {
+//                     // Enable button & show success message
+//                     $("#btnSubmit").attr("disabled", false);
+//                     $('#sdoctoruccess').html("<div class='alert alert-success'>");
+//                     $('#doctorsuccess > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+//                         .append("</button>");
+//                     $('#doctorsuccess > .alert-success')
+//                         .append("<strong>You will receive an email shortly. </strong>");
+//                     $('#doctorsuccess > .alert-success')
+//                         .append('</div>');
+//
+//                     //clear all fields
+//                     $('#doctorForm').trigger("reset");
+//                 },
+//                 error: function() {
+//                     // Fail message
+//                     $('#doctorsuccess').html("<div class='alert alert-danger'>");
+//                     $('#doctorsuccess > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+//                         .append("</button>");
+//                     $('#doctorsuccess > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+//                     $('#doctorsuccess > .alert-danger').append('</div>');
+//                     //clear all fields
+//                     $('#doctorForm').trigger("reset");
+//                 },
+//             });
+//         }
+//
+//         function contact_submit() {
+//             // Prevent spam click and default submit behaviour
+//             $("#btnSubmit").attr("disabled", true);
+//             //event.preventDefault();
+//             grecaptcha.execute();
+//
+//
+//             // get values from FORM
+//             var name = $("input#name").val();
+//             var email = $("input#email").val();
+//             var subject = $("input#subject").val();
+//             var message = $("textarea#message").val();
+//             var firstName = name; // For Success/Failure Message
+//             // Check for white space in name for Success/Fail message
+//             if (firstName.indexOf(' ') >= 0) {
+//                 firstName = name.split(' ').slice(0, -1).join(' ');
+//             }
+//             $.ajax({
+//                 url: "/contact",
+//                 type: "POST",
+//                 data: {
+//                     name: name,
+//                     email: email,
+//                     subject: subject,
+//                     message: message,
+//                 },
+//                 cache: false,
+//                 success: function() {
+//                     // Enable button & show success message
+//                     $("#btnSubmit").attr("disabled", false);
+//                     $('#success').html("<div class='alert alert-success'>");
+//                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+//                         .append("</button>");
+//                     $('#success > .alert-success')
+//                         .append("<strong>You will receive an email shortly. </strong>");
+//                     $('#success > .alert-success')
+//                         .append('</div>');
+//
+//                     //clear all fields
+//                     $('#contactForm').trigger("reset");
+//                 },
+//                 error: function() {
+//                     // Fail message
+//                     $('#success').html("<div class='alert alert-danger'>");
+//                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+//                         .append("</button>");
+//                     $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+//                     $('#success > .alert-danger').append('</div>');
+//                     //clear all fields
+//                     $('#contactForm').trigger("reset");
+//                 },
+//             });
+//         }
+//
+// $(function() {
+//
+//     $("#patientForm input,#patientForm textarea").jqBootstrapValidation({
+//         preventSubmit: true,
+//         submitError: function($form, event, errors) {
+//             // additional error messages or events
+//         },
+//         submitSuccess: patient_submit(),
+//         filter: function() {
+//             return $(this).is(":visible");
+//         },
+//     });
+//
+//
+//
+//     $("#doctorForm input,#doctorForm textarea").jqBootstrapValidation({
+//         preventSubmit: true,
+//         submitError: function($form, event, errors) {
+//             // additional error messages or events
+//         },
+//         submitSuccess: doctor_submit(),
+//         filter: function() {
+//             return $(this).is(":visible");
+//         },
+//     });
+//
+//
+//         $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
+//         preventSubmit: true,
+//         submitError: function($form, event, errors) {
+//             // additional error messages or events
+//         },
+//         submitSuccess: contact_submit(),
+//         filter: function() {
+//             return $(this).is(":visible");
+//         },
+//     });
+//
+//     $("a[data-toggle=\"tab\"]").click(function(e) {
+//         e.preventDefault();
+//         $(this).tab("show");
+//     });
+// });
 
 // When clicking on Full hide fail/success boxes
 $('#name').focus(function() {
